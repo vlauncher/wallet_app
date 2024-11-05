4351cb1fddac6847d55b29dbe8c2747518b04b6d
vlauncher <v2dent@gmail.com>
2024-11-05 08:56:21 +0100

    added tests setup and refractored wallet app

--- src/routes/wallet.routes.ts
+++ src/routes/wallet.routes.ts
@@ -1,12 +1,14 @@
-import { Router } from "express";
-import { getWalletBalance, fundWallet, transferFundsToUser, withdrawFromAccount } from "../controllers/wallet.controllers";
-import { auth } from "../middlewares/auth";
+// src/routes/wallet.routes.ts
+import { Router } from 'express';
+import { WalletController } from '../controllers/wallet.controllers';
+import { auth } from '../middlewares/auth';
 
 const router = Router();
+const walletController = new WalletController();
 
-router.get('/balance', auth, getWalletBalance);
-router.post('/fund',auth, fundWallet);
-router.post('/transfer',auth, transferFundsToUser);
-router.post('/withdraw', auth, withdrawFromAccount);
+router.get('/balance', auth, walletController.getWalletBalance.bind(walletController));
+router.post('/fund', auth, walletController.fundWallet.bind(walletController));
+router.post('/transfer', auth, walletController.transferFundsToUser.bind(walletController));
+router.post('/withdraw', auth, walletController.withdrawFromAccount.bind(walletController));
 
 export default router;
